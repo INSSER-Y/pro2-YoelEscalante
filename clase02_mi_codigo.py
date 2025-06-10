3d0e22224a955f3a09067feb5caad252ebc226b9
INSSER <yoel122000@gmail.com>
2025-06-10 01:07:10 +0000

    primer cambio ejercicio1

--- clase02_mi_codigo.py
+++ clase02_mi_codigo.py
@@ -1,3 +1,15 @@
 print(¡Estoy aprendiendo Git y Github en Prog II!)
 nombre = Yoel Escalante 
 print(fEste commit lo esta haciendo: {Yoel})
+#ejercicio1
+"""Estudiante Yoel Escalante Escobar"""
+print("Escriba su edad")
+edad = int(input())
+if edad >= 18:
+    print("Puedes ver peliculas clasificacion R!")
+if edad >= 13 and edad < 18:
+    print("Puedes ver peliculas clasificacion PG-13!")
+if edad < 13 and edad >=0:
+  print("Te recomendamos ver peliculas clasificacion G o PG!")
+if edad < 0:
+  print("Edad no valida!")
