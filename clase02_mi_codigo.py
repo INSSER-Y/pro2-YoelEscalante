9adda68744f13df0d90594d9df52031743b3760e
INSSER <yoel122000@gmail.com>
2025-06-10 13:19:07 +0000

    segundo ejercicio subido

--- clase02_mi_codigo.py
+++ clase02_mi_codigo.py
@@ -28,3 +28,10 @@ while contador < num:
     print (f"contador es : {contador}")
     contador+=1
 print("Bucle while terminado")
+#ejercicio2
+num_tabla = int(input("Ingrese un numero para ver su tabla de multiplicar: "))
+print(f"---Tabla del {num_tabla}---")
+for i in range(1,11):
+    resultado = num_tabla * i
+    print(f"{num_tabla} x {i} = {resultado}")
+    
