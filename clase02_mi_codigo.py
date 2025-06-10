e0fe28230fc71c48546654cd4b556595a5850e92
INSSER <yoel122000@gmail.com>
2025-06-10 13:54:52 +0000

    subido y ordenado todos los ejercicios hasta el 6

--- clase02_mi_codigo.py
+++ clase02_mi_codigo.py
@@ -1,7 +1,25 @@
 """print(¡Estoy aprendiendo Git y Github en Prog II!)
 nombre = Yoel Escalante 
 print(fEste commit lo esta haciendo: {Yoel})"""
-#ejercicio1
+
+#practica
+#ejercicio2
+print("Recorriendo un String")
+for numero in range(3):
+    print(numero)
+print ("Recorriendo un String:")
+nombre = (input())
+for letra in nombre:
+    print(letra) 
+#ejericicio3
+print ("Bucle while:")
+num = int(input())
+contador = 0
+while contador < num:
+    print (f"contador es : {contador}")
+    contador+=1
+print("Bucle while terminado")
+#ejercicio4
 """Estudiante Yoel Escalante Escobar"""
 print("Escriba su edad")
 edad = int(input())
@@ -13,25 +31,20 @@ if edad < 13 and edad >=0:
   print("Te recomendamos ver peliculas clasificacion G o PG!")
 if edad < 0:
   print("Edad no valida!")
-#practica
-print("Recorriendo un String")
-for numero in range(3):
-    print(numero)
-print ("Recorriendo un String:")
-nombre = (input())
-for letra in nombre:
-    print(letra)    
-print ("Bucle while:")
-num = int(input())
-contador = 0
-while contador < num:
-    print (f"contador es : {contador}")
-    contador+=1
-print("Bucle while terminado")
-#ejercicio2
+#ejercicio5
 num_tabla = int(input("Ingrese un numero para ver su tabla de multiplicar: "))
 print(f"---Tabla del {num_tabla}---")
 for i in range(1,11):
     resultado = num_tabla * i
     print(f"{num_tabla} x {i} = {resultado}")
-    
+#ejercicio6
+sec = 6
+adiv = int(input(f"Adivina el numero: "))
+while adiv != sec:
+    if adiv > sec:
+        print("El numero es demasiado alto")
+    if adiv < sec:
+        print("El numero es demasiado bajo")
+    adiv = int(input(f"Vuelva a intentarlo : "))
+if adiv == sec:
+    print("Adivinaste!")
