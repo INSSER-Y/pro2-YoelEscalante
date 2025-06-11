46d1f11c059e3469ad3a1ce7669117db263a91cb
INSSER <yoel122000@gmail.com>
2025-06-11 13:30:14 +0000

    Ejercicio 4

--- clase04_listas.py
+++ clase04_listas.py
@@ -14,3 +14,16 @@ nombres_estudiantes = ['Carlos', 'Ana', 'Miguel', 'Sofia', 'Diego']
 for nombre in nombres_estudiantes:
   print(f"Bienvenido al equipo, {nombre}!")
 print()
+##Crear una lista de notas numericas
+mis_notas = [85.5,92,78,88.5,95,82]
+#inicializar Variable para la suma
+suma_total = 0
+#Usar un bucle for para calcular la suma total sin usar sum()
+for nota in mis_notas:
+  suma_total += nota
+#calcular el promedio
+promedio = suma_total / len(mis_notas)
+#Imprimir resultados de forma clara
+print(f"Suma total de las notas: {suma_total}")
+print(f"Promedio de las notas de Yoel son: {promedio:.2f}")
+print()
