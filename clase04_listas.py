b1a1daac321e9659964a8d46d7f13bece0841bd2
INSSER <yoel122000@gmail.com>
2025-06-11 12:57:50 +0000

    clase 04 añadido

---
+++ clase04_listas.py
@@ -0,0 +1,16 @@
+#Listas 
+#Listas de hobbys
+hobbys = ["leer", "viajar", "jugar videojuegos", "escuchar musica", "ver peliculas"]
+for nombre in hobbys:
+    print(f"Mis hobbis son {nombre}")
+print()
+#Listas de comidas favoritas
+comidas_favoritas = ["pizza", "hamburguesas", "tacos", "sushi", "pasta"]
+for nombre in comidas_favoritas:
+    print(f"Mis comidas favoritas son {nombre}")
+print()
+#Listas de nombres de estudiantes
+nombres_estudiantes = ['Carlos', 'Ana', 'Miguel', 'Sofia', 'Diego']
+for nombre in nombres_estudiantes:
+  print(f"Bienvenido al equipo, {nombre}!")
+print()
