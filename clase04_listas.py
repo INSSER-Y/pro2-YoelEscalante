1368175e2c302a3a636fc86c316ec0b3d294b8ed
INSSER <yoel122000@gmail.com>
2025-06-13 13:21:11 +0000

    ordenado los ejercicio

--- clase04_listas.py
+++ clase04_listas.py
@@ -24,7 +24,7 @@ for nota in mis_notas:
 #calcular el promedio
 promedio = suma_total / len(mis_notas)
 #Imprimir resultados de forma clara
-print(f"Suma total de las notas: {suma_total}")
+print(f"Suma total de mis notas {mis_notas} : {suma_total}")
 print(f"Promedio de las notas de Yoel son: {promedio:.2f}")
 print()
 #suma de elementos de un vectores
@@ -40,8 +40,9 @@ assert suma_elementos([10, -2, 5]) == 13
 assert suma_elementos([]) == 0 # ¡Importante probar con una lista vacía!
 assert suma_elementos([100]) == 100
 print("¡Pruebas para sumar_elementos pasaron! ")
-resultado = suma_elementos([1,5,4])
-print (resultado)
+vector = [1, 5, 4]
+resultado = suma_elementos(vector)
+print(f"La suma del vector {vector} es: {resultado}")
 print()
 #Encontrar el numero mayor de una lista
 def encontrar_mayor(lista):
@@ -52,8 +53,6 @@ def encontrar_mayor(lista):
         if numero > mayor:
             mayor = numero
     return mayor
-resultado = encontrar_mayor([1, 2, 11, 4, 5, 6, 7, 8, 9, 10])
-print (resultado)
 print("\nProbando encontrar_mayor...")
 assert encontrar_mayor([1, 9, 2, 8, 3, 7]) == 9
 assert encontrar_mayor([-1, -9, -2, -8]) == -1
@@ -61,6 +60,9 @@ assert encontrar_mayor([42, 42, 42]) == 42
 assert encontrar_mayor([]) == None # Prueba del caso especial
 assert encontrar_mayor([5]) == 5
 print("¡Pruebas para encontrar_mayor pasaron! ")
+vector = [1, 5, 4]
+resultado = encontrar_mayor(vector)
+print(f"El numero mayor del vector {vector} es: {resultado}")
 print()
 
 #contar cuantas veces hay un elemento en una lista
@@ -71,9 +73,9 @@ def contar_elemento(lista, elemento):
             contador += 1
     if contador == 0:
         return f"El elemento {elemento} no se encuentra en la lista"
-    return f"El elemento {elemento} se repite {contador} veces"
-resultado = contar_elemento([1, 2, 3, 4, 5, 6, 5, 8, 9, 10], 5)
-print (resultado)
+    return f"El elemento {elemento} de la lista {lista} se repite {contador} veces"
+resultado= contar_elemento([1, 2, 3, 4, 5, 6, 7, 5, 9, 10], 5)
+print(resultado)
 print()
 #invertir el orden de una lista
 def lista_invertida(lista):
@@ -81,8 +83,6 @@ def lista_invertida(lista):
     for i in range(len(lista)-1, -1, -1):
         lista_invertida.append(lista[i])
     return lista_invertida
-resultado = lista_invertida([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-print(resultado)
 print("\nProbando invertir_lista...")
 lista_prueba = [1, 2, 3, 4, 5]
 lista_resultante = lista_invertida(lista_prueba)
@@ -91,6 +91,9 @@ assert lista_prueba == [1, 2, 3, 4, 5] # ¡Verifica que la original no cambió!
 assert lista_invertida(["a", "b", "c"]) == ["c", "b", "a"]
 assert lista_invertida([]) == []
 print("¡Pruebas para invertir_lista pasaron! ")
+vector = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+resultado = lista_invertida(vector)
+print(f"Invertir la lista {vector} : la lista invertida es {resultado}")
 print()
 #busqueda secuencial o lineal
 def busqueda_lineal(lista, elemento):
@@ -107,8 +110,12 @@ assert busqueda_lineal(mi_lista_desordenada, 25) == 6 # Al final
 assert busqueda_lineal(mi_lista_desordenada, 99) == -1 # No existe
 assert busqueda_lineal([], 5) == -1
 print("¡Pruebas para busqueda_lineal pasaron! ")
-resultado = busqueda_lineal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)
-print (resultado)
+vector = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+elemento = 5
+resultado = busqueda_lineal(vector, elemento)
+if resultado == -1:
+    print(f"Buscar si el {elemento} esta en la lista {vector} : No se a encontrado el {elemento}")
+print(f"Buscar si el {elemento} esta en la lista {vector} : Si se a encontrado el {resultado}")
 #busqueda binaria
 def busqueda_binaria(lista, elemento):
     izquierda, derecha = 0, len(lista) - 1
@@ -129,3 +136,10 @@ assert busqueda_binaria(lista_ordenada, 2) == 0 # Primero
 assert busqueda_binaria(lista_ordenada, 3) == -1 # No existe
 assert busqueda_binaria(lista_ordenada, 100) == -1 # Fuera de rango (mayor)
 print("¡Pruebas para busqueda_binaria pasaron! ")
+Lista = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+elemento = 23
+resultado = busqueda_binaria(Lista, elemento)
+if resultado == -1:
+    print(f"Buscar si el {elemento} esta en la lista {Lista} : No se a encontrado el {elemento}")
+print(f"Buscar si el {elemento} esta en la lista {Lista} : Si se a encontrado el {resultado}")
+print()
