06fd9460a923eba7d74154302c6e7c13079c3964
INSSER <yoel122000@gmail.com>
2025-06-18 13:38:53 +0000

    matrices añadida

--- clase08_ordenamiento_avanzado.py
+++ clase08_ordenamiento_avanzado.py
@@ -54,5 +54,52 @@ assert merge_sort([100, -50, 0, 50, -100]) == [-100, -50, 0, 50, 100]  # Lista c
 assert merge_sort([2.5, 1.2, 3.8]) == [1.2, 2.5, 3.8]  # Lista con flotantes
 print("¡Todas las pruebas con assert pasaron correctamente!")
 
-print("JIM REQUENA - FIN DEL PROGRAMA")
 print()
+
+# matrices 
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+#opcion recorriendo con indices
+num_filas = len(matriz)
+num_columnas = len(matriz[0])
+for i in range(num_filas):#bucle exterior para fila (0,1,2)
+    for j in range(num_columnas):#bucle interior para indices de columna (0,1,2)
+        elemento = matriz[i][j]
+        print(f"Elemento en la fila {i}, columna {j}: {elemento}")
+print()
+#opcion 2: recorriendo por elemento (mas "pythonico")
+for fila_actual in matriz: #bucle exterior toma cada lista-fila
+    for elemento in fila_actual:#buecle interior toma cada elemento de esa fila
+        print(elemento, end=" ")#"end=' '" para imprimir en la misma linea
+    print()#salto de linea al final de cada fila
+print()
+#---------------------------
+#ejercicio 1 Modelando un teclado numerico
+#---------------------------
+teclado = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+for fila_actual in teclado:
+    for elemento in fila_actual:
+        print(elemento, end=" ")
+    print()
+for fila_actual in teclado:
+    for elemento in fila_actual:
+        print(elemento, end="\t")
+    print()
+vacio = []
+for i in range(5):
+    fila = []
+    for j in range(5):
+        fila.append(0)
+    vacio.append(fila)
+    print()
+for fila in vacio:
+    for elemento in fila:
+        print(elemento, end="\t")
+    print()
